901284e12306bc4e287e3150b372917021840c50
LeoMiguelB <108880352+LeoMiguelB@users.noreply.github.com>
2023-06-25 19:41:58 -0400

    need to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,26 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Personal Portfolio',
-  description: 'This is a website that showcases my journey',
+export const metadata: Metadata = {
+  generator: 'Next.js',
+  title: "Personal Portfolio",
+  description: "Leo studies computer science at University of Guelph",
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  verification: {
+    google: 'google',
+    other: {
+      me: ['leo.miguel.bantl@gmail.com'],
+    },
+  },
+  keywords: ['web dev', 'web development', 'personal portfolio'],
 }
-
 export default function RootLayout({
   children,
 }: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,12 @@ import { useState, useEffect } from "react";
 import Hero from "@/components/sections/Hero"
 import styles from "./index.module.css";
 import { Montserrat } from "next/font/google";
-import { Metadata } from "next";
 import Navigation from "@/components/Navigation";
 import About from "@/components/sections/About";
 import useAxios from "../../hooks/useAxios";
 import Skills from "@/components/sections/Skills";
 import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
-
-export const metadata: Metadata = {
-  generator: 'Next.js',
-  title: "Personal Portfolio",
-  description: "Leo studies computer science at University of Guelph",
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-  },
-  verification: {
-    google: 'google',
-    other: {
-      me: ['leo.miguel.bantl@gmail.com'],
-    },
-  },
-  keywords: ['web dev', 'web development', 'personal portfolio'],
-}
 
 const inter = Montserrat({ subsets: ["latin"] });
 
